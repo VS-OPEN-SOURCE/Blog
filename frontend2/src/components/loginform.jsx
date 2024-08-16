b7ed9145cf19bdbd3afdb3ba8247f008b2110382
Venuchander <venuchander1@gmail.com>
2024-08-16 15:08:51 +0530

    Resolve case sensitive

--- frontend2/src/components/loginform.jsx
+++ frontend2/src/components/loginform.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import Button from './button';
+import Button from './Button';
 
 const Navbar = () => {
   const [user, setUser] = useState(null);
